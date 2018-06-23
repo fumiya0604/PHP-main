40e59991c8740ab2ee9b6cab0ca56b42d3b7b9a1
fumiya <fumiya083077@gmail.com>
2018-06-23 16:13:39 +0900

    初回コミット

--- main.php
+++ main.php
@@ -1,4 +1,13 @@
 #!C:\php\php.exe
 <?php
 print("HelloWorld");
+echo "aa\r\n";
+echo "cc";
+for ($i = 1; $i <= 5; $i++) {
+    echo $i;
+    echo "\r\n";
+    print "\r\n";
+    # コメント
+    // コメント
+}
 ?>
